8e9bb26ed833c69b2795bd98ea8c1bc3ffa59582
andrewdoss <doss@bit.io>
2021-09-21 11:09:12 -0700

    Fix bit.io username

--- simple_pipeline/ca_covid_data.sql
+++ simple_pipeline/ca_covid_data.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS "action_tester/simple_pipeline"."ca_covid_data_join" (
+CREATE TABLE IF NOT EXISTS "bitdotio/simple_pipeline"."ca_covid_data_join" (
   date date,
   county TEXT,
   fips TEXT,
@@ -13,9 +13,9 @@ CREATE TABLE IF NOT EXISTS "action_tester/simple_pipeline"."ca_covid_data_join" 
   deaths_per_total_population REAL
 );
 
-TRUNCATE TABLE "action_tester/simple_pipeline"."ca_covid_data_join";
+TRUNCATE TABLE "bitdotio/simple_pipeline"."ca_covid_data_join";
 
-INSERT INTO "action_tester/simple_pipeline"."ca_covid_data_join"
+INSERT INTO "bitdotio/simple_pipeline"."ca_covid_data_join"
   SELECT
     CD.date,
     CD.county,
@@ -29,10 +29,10 @@ INSERT INTO "action_tester/simple_pipeline"."ca_covid_data_join"
     POP.population_65plus,
     1.0 * CD.cases / POP.total_population AS cases_per_total_population,
     1.0 * CD.deaths / POP.total_population AS deaths_per_total_population
-  FROM "action_tester/simple_pipeline"."cases_counties" AS CD
-  LEFT JOIN "action_tester/simple_pipeline"."vaccinations_counties" AS VACCS
+  FROM "bitdotio/simple_pipeline"."cases_counties" AS CD
+  LEFT JOIN "bitdotio/simple_pipeline"."vaccinations_counties" AS VACCS
   ON CD.fips = VACCS.fips
-  LEFT JOIN "action_tester/simple_pipeline"."population_counties" AS POP
+  LEFT JOIN "bitdotio/simple_pipeline"."population_counties" AS POP
   ON CD.fips = POP.fips
   WHERE CD.date >= '2021-05-01' AND CD.fips ~ '^06'
   ORDER BY CD.fips ASC, CD.date DESC;
