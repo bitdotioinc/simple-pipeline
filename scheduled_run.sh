7a731c0df19f9f88e4c7f3da722c7a5e9fe29ea0
Stephan Klinger <staeff@arcor.de>
2022-02-14 23:46:28 +0100

    Call sql_executor with user and repo

--- scheduled_run.sh
+++ scheduled_run.sh
@@ -6,4 +6,4 @@ cd simple_pipeline
 # python main.py -local_source -name acs_population_counties acs_5yr_population_data.csv bitdotio/simple_pipeline.population_counties
 python main.py -name nyt_cases_counties 'https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv' bitdotio/simple_pipeline.cases_counties
 python main.py -name cdc_vaccines_counties 'https://data.cdc.gov/api/views/8xkx-amqh/rows.csv?accessType=DOWNLOAD' bitdotio/simple_pipeline.vaccinations_counties
-python sql_executor.py ca_covid_data.sql
+python sql_executor.py ca_covid_data.sql bitdotio simple_pipeline
